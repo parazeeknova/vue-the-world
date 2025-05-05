f43d854622d284e2dd4f0bb55d4c91825bac9d08
Harsh <yesh8harsh+github@gmail.com>
2025-05-05 18:39:23 +0530

    feat[DB]: Setup drizzle and turso db for sqlite (#54)

--- lib/env.ts
+++ lib/env.ts
@@ -5,6 +5,8 @@ import tryParseEnv from "./try-parse-env";
 const EnvSchema = z.object({
   NODE_ENV: z.string(),
   COREPACK_ENABLE_AUTO_PIN: z.string(),
+  TURSO_DATABASE_URL: z.string(),
+  TURSO_AUTH_TOKEN: z.string(),
 });
 
 export type EnvSchema = z.infer<typeof EnvSchema>;
