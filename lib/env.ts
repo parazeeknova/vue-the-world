afff99278d70e6e44ceea6690c73e7815ef0b22a
Harsh Sahu <yesh8harsh+github@gmail.com>
2025-05-05 19:20:04 +0530

    feat[AUTH]: Auth schema for better auth and lil setup

--- lib/env.ts
+++ lib/env.ts
@@ -7,6 +7,8 @@ const EnvSchema = z.object({
   COREPACK_ENABLE_AUTO_PIN: z.string(),
   TURSO_DATABASE_URL: z.string(),
   TURSO_AUTH_TOKEN: z.string(),
+  BETTER_AUTH_SECRET: z.string(),
+  BETTER_AUTH_URL: z.string(),
 });
 
 export type EnvSchema = z.infer<typeof EnvSchema>;
